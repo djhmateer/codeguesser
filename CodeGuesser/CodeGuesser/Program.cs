87e4e0e5da06de04601ef629426536e706ccceb7
Dave Mateer <davemateer@gmail.com>
2020-03-01 10:24:33 +0000

    asdf

---
+++ CodeGuesser/CodeGuesser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeGuesser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
